e02bcc89616992f5c90826e90bdc823cb80edf1f
Bernard Lee (2002461) <72250096+cklee23@users.noreply.github.com>
2021-11-08 20:22:05 +0800

    upload blockly js

---
+++ app/static/js/control2js.js
@@ -0,0 +1,51 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function() {
+
+  let currentButton;
+
+  function handlePlay(event) {
+    // Add code for playing sound.
+  }
+
+  function save(button) {
+    // Add code for saving the behavior of a button.
+  }
+
+  function handleSave() {
+    document.body.setAttribute('mode', 'edit');
+    save(currentButton);
+  }
+
+  function enableEditMode() {
+    document.body.setAttribute('mode', 'edit');
+    document.querySelectorAll('.button').forEach(btn => {
+      btn.removeEventListener('click', handlePlay);
+      btn.addEventListener('click', enableBlocklyMode);
+    });
+  }
+
+  function enableMakerMode() {
+    document.body.setAttribute('mode', 'maker');
+    document.querySelectorAll('.button').forEach(btn => {
+      btn.addEventListener('click', handlePlay);
+      btn.removeEventListener('click', enableBlocklyMode);
+    });
+  }
+
+  function enableBlocklyMode(e) {
+    document.body.setAttribute('mode', 'blockly');
+    currentButton = e.target;
+  }
+
+  document.querySelector('#edit').addEventListener('click', enableEditMode);
+  document.querySelector('#done').addEventListener('click', enableMakerMode);
+  document.querySelector('#save').addEventListener('click', handleSave);
+
+  enableMakerMode();
+
+})();
